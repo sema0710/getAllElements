19442919231b8331b2703b097dd7b3528694305a
sema0710 <pandati@naver.com>
2019-12-20 08:58:22 +0900

    getAllElemetns
    getAllElements by one command

---
+++ getAllElements.js
@@ -0,0 +1,28 @@
+export const getAllElementById = ()=>{
+    const all = document.querySelectorAll("*");
+    let id_elements = {};
+    for(i of all){
+        let id = i.id;
+        if(id !== ""){
+            id_elements[id] = i;
+        }
+    }
+    return id_elements;
+}
+
+export const getAllElementByClass = ()=>{
+    let class_elements = {};
+    const all = document.querySelectorAll("*");
+    for(i of all){
+        let Class = i.classList;
+        if(Class.value !== ""){
+            for(j of Class){
+                if(class_elements[j] === undefined){
+                    class_elements[j] = [];
+                }
+                class_elements[j].push(i);
+            }
+        }   
+    }
+    return class_elements;
+}
